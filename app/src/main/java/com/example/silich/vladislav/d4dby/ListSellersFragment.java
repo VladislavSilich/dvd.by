d4e67e38f6cf8dae54aab493e1c034c6102661be
vlad <vladislav.silich.1996@gmail.com>
2017-11-08 15:28:50 +0300

    create list sellers

---
+++ app/src/main/java/com/example/silich/vladislav/d4dby/ListSellersFragment.java
@@ -0,0 +1,22 @@
+package com.example.silich.vladislav.d4dby;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ListSellersFragment extends android.app.Fragment {
+
+
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_list_sellers, container, false);
+	}
+
+
+}
